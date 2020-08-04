012abab7143334e94e3f5930bf650882ff980332
Surya <suryakumar.y@quiddityit.com>
2020-08-04 21:31:00 +0530

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { InvoiceComponent } from './pages/invoice/invoice.component';
+import { SalesComponent } from './pages/sales/sales.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SpendsComponent } from './pages/spends/spends.component';
+import { InvoiceSampleComponent } from './pages/invoice-sample/invoice-sample.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InvoiceComponent,
+    SalesComponent,
+    SignupComponent,
+    SpendsComponent,
+    InvoiceSampleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/invoice-sample/invoice-sample.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-sample',
+  templateUrl: './invoice-sample.component.html',
+  styleUrls: ['./invoice-sample.component.scss']
+})
+export class InvoiceSampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
